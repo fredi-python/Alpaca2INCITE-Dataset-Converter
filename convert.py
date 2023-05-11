a26d4be3cb976af1774e1d112727502893bdcbde
fredithefish <83492589+fredi-python@users.noreply.github.com>
2023-05-11 12:39:43 +0200

    Add files via upload

---
+++ convert.py
@@ -0,0 +1,19 @@
+import json
+import argparse
+
+parser = argparse.ArgumentParser(description='Alpaca2INCITE-Dataset-Converter')
+parser.add_argument('--input_file', type=str, help='the input JSON file')
+parser.add_argument('--output_file', type=str, help='the output JSONL file')
+parser.add_argument('--source', type=str, help='the source metadata for the JSONL entries', required=False)
+args = parser.parse_args()
+
+with open(args.input_file) as f:
+    data = json.load(f)
+
+with open(args.output_file, 'w') as f:
+    for item in data:
+        text = f"<human>: {item['instruction']}\n<bot>: {item['output']}"
+        output = {"text": text}
+        if args.source:
+            output["metadata"] = {"source": args.source}
+        f.write(json.dumps(output) + '\n')
